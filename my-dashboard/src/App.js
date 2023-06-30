cbccf1c0329792f81086ceb766d28670f9e25bba
DamiJ <damianojongerius@gmail.com>
2023-06-30 12:12:19 +0200

    done

--- my-dashboard/src/App.js
+++ my-dashboard/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import MoneyLeaderBoard from './components/MoneyLeaderboard';
+import PlayTimeLeaderBoard from './components/PlayTimeLeaderBoard';
 import './styles/index.css';
 
 function App() {
   return (
     <div className="app">
+      <h1>factory leaderboards</h1>
+      <div className='leaderboard-container'>
       <MoneyLeaderBoard />
-      <div> //leaderboard playtime
-
-      </div>
-      <div> //list of players with profiles
-
+      <PlayTimeLeaderBoard />
       </div>
     </div>
   );
--- my-dashboard/src/components/PlayTimeLeaderBoard.js
+++ my-dashboard/src/components/PlayTimeLeaderBoard.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function MoneyLeaderBoard() {
+function PlayTimeLeaderBoard() {
   const [leaderboardData, setLeaderboardData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:8080/Leaderboard/Money');
+        const response = await axios.get('http://localhost:8080/Leaderboard/Playtime');
         console.log(response);
         setLeaderboardData(response.data);
       } catch (error) {
@@ -20,13 +20,13 @@ function MoneyLeaderBoard() {
 
   return (
 <div className="leaderboard">
-      <h1 className="leaderboard-title">Money</h1>
-      <table className="leaderboard-table">
+      <h1 className="leaderboard-title">playTime</h1>
+      <table className="leaderboard-table">  
         <thead>
           <tr>
             <th>Username</th>
             <th>Save Name</th>
-            <th>Money</th>
+            <th>playtime</th>
           </tr>
         </thead>
         <tbody>
@@ -34,7 +34,7 @@ function MoneyLeaderBoard() {
             <tr key={index}>
               <td>{entry.userName}</td>
               <td>{entry.saveName}</td>
-              <td>{entry.money}</td>
+              <td>{entry.playtime}</td>
             </tr>
           ))}
         </tbody>
@@ -43,4 +43,4 @@ function MoneyLeaderBoard() {
   );
 }
 
-export default MoneyLeaderBoard;
+export default PlayTimeLeaderBoard;
